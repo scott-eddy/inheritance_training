d7d4a72152ae53814c8cc5b426c7c7d28829b358
Eddy Scott <escott@morse-corp.com>
2024-10-09 22:44:58 -0400

    Add default visual studio project

---
+++ inheritance/src/main/java/sushi/training/example/App.java
@@ -0,0 +1,13 @@
+package sushi.training.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
